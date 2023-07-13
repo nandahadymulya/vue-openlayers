afec2de2834bcd4d8a834fe6fe185226c224c3e7
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2023-07-13 13:43:57 +0700

    feat: changed purge to content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  // content: [
-  //   // Example content paths...
-  //   './public/**/*.html',
-  //   './src/**/*.{js,jsx,ts,tsx,vue}'
-  // ],
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}'
+  ],
   fontFamily: {
     sans: ['Plus Jakarta Sans', 'Sans-serif']
   },
