178361df50124a1e2de2c9d540959c9440c37f48
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2023-07-13 21:03:49 +0700

    feat: add func input new location

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,37 +1,65 @@
 <script setup>
 import { computed } from 'vue'
+import { useLocationStore } from '../stores/location'
+
+const locationStore = useLocationStore()
 
 const props = defineProps({
   name: String,
   longitude: Number,
   latitude: Number
 })
+
 defineEmits(['update:name', 'update:longitude', 'update:latitude'])
 
-const propsValue = computed({
+const value = computed({
   get() {
-    return props
+    return {
+      name: props.name,
+      longitude: props.longitude,
+      latitude: props.latitude
+    }
   },
-  set(value) {
-    emit('update:modelValue', value)
+  set(val) {
+    console.log(val)
+    emit('update:name', val)
+    emit('update:latitude', val)
+    emit('update:longitude', val)
   }
 })
 
 const addLocationToJsonFile = () => {
-  const newData = {
-    name: props.name,
-    longitude: props.longitude,
-    latitude: props.latitude
+  const inputedLocation = {
+    name: value.value.name,
+    longitude: value.value.longitude,
+    latitude: value.value.latitude
   }
-  console.log(newData)
+
+  console.log(inputedLocation)
+
+  try {
+    locationStore.createLocation(inputedLocation)
+  } catch (error) {
+    console.log(error)
+  }
 }
 </script>
 
 <template>
   <div class="flex justify-center gap-2">
-    <input v-model="props.name" class="border p-1 rounded" placeholder="Name" />
-    <input v-model="props.longitude" class="border p-1 rounded" placeholder="Latitude" />
-    <input v-model="props.latitude" class="border p-1 rounded" placeholder="Longitude" />
+    <input v-model="value.name" type="text" class="border p-1 rounded" placeholder="Name" />
+    <input
+      v-model="value.longitude"
+      type="number"
+      class="border p-1 rounded"
+      placeholder="Latitude"
+    />
+    <input
+      v-model="value.latitude"
+      type="number"
+      class="border p-1 rounded"
+      placeholder="Longitude"
+    />
     <button @click="addLocationToJsonFile" class="bg-orange-500 text-white px-2 py-1 rounded">
       Add New Location
     </button>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,21 @@
 <script setup>
 import MapComponent from '@/components/MapComponent.vue'
 import InputComponent from '@/components/InputComponent.vue'
+import { ref, computed, reactive } from 'vue'
 
-const newData = {
-  name: 'Test',
-  longitude: 5.54,
-  latitude: 1.54
+const inputed = {
+  name: 'Lokasi D',
+  latitude: -7.8941,
+  longitude: 110.3811
 }
 </script>
 <template>
   <div>
     <h1 class="my-3 text-center text-xl font-bold">Vue 3 + Vite + Tailwind CSS + OpenLayers</h1>
     <InputComponent
-      :name="newData.name"
-      :longitude="newData.longitude"
-      :latitude="newData.latitude"
+      v-model:name="inputed.name"
+      v-model:longitude="inputed.longitude"
+      v-model:latitude="inputed.latitude"
     />
     <MapComponent />
   </div>
